dae759d22521b55ef0f0c182c59b1980d98cb06b
jjoo1229 <110160016+jjoo1229@users.noreply.github.com>
2026-01-29 23:40:35 +0900

    [Silver IV] Title: 큐, Time: 176 ms, Memory: 19856 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static Queue<Integer> queue = new LinkedList<>();
+    static StringBuilder sb = new StringBuilder();
+    static int last;
+    
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String line = st.nextToken();
+            
+            if (line.equals("push")) {
+                int value = Integer.parseInt(st.nextToken());
+                queue.offer(value);
+                last = value;
+            } else {
+                handleQueue(line);
+            }
+        }
+        System.out.print(sb);
+    }
+
+    public static void handleQueue(String command) {
+        switch (command) {
+            case "pop":
+                sb.append(queue.isEmpty() ? -1 : queue.poll()).append('\n');
+                break;
+            case "size":
+                sb.append(queue.size()).append('\n');
+                break;
+            case "empty":
+                sb.append(queue.isEmpty() ? 1 : 0).append('\n');
+                break;
+            case "front":
+                sb.append(queue.isEmpty() ? -1 : queue.peek()).append('\n');
+                break;
+            case "back":
+                sb.append(queue.isEmpty() ? -1 : last).append('\n');
+                break;
+        }
+    }
+}
