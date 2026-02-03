1f609809efb8f1a984f1983986c04d0c06490607
jjoo1229 <110160016+jjoo1229@users.noreply.github.com>
2026-02-03 11:52:41 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 95 ms, Memory: 60,032 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
@@ -1,19 +1,39 @@
-for _ in range(10):
-    test_case = int(input())
-    graph = [list(map(int, input().split())) for _ in range(100)]
-    startX = 99
-    startY = graph[99].index(2)
+import java.util.*;
+import java.io.*;
 
-    while startX > 0:
-        if startY - 1 >= 0 and graph[startX][startY - 1] == 1:
-            while startY - 1 >= 0 and graph[startX][startY - 1] == 1:
-                startY -= 1
-            startX -= 1
-        elif startY + 1 < 100 and graph[startX][startY + 1] == 1:
-            while startY + 1 < 100 and graph[startX][startY + 1] == 1:
-                startY += 1
-            startX -= 1
-        else:
-            startX -= 1
+class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        for (int test_case = 1; test_case <= 10; test_case++) {
+    		sc.nextInt();
+            
+            int[][] arr = new int[100][100];
+            int startX = 99, startY = 0;
+            for (int i = 0; i < 100; i++) {
+                for (int j = 0; j < 100; j++) {
+            		arr[i][j] = sc.nextInt();
+                    if (arr[99][j] == 2) {
+                        startY = j;
+                    }
+                }
+            }
 
-    print(f"#{test_case} {startY}")
+            int nx = startX, ny = startY;
+
+            while (nx > 0) {
+                nx--;
+
+                if (ny - 1 >= 0 && arr[nx][ny - 1] == 1) {
+                    while (ny - 1 >= 0 && arr[nx][ny - 1] == 1) {
+                        ny--;
+                    }
+                } else if (ny + 1 < 100 && arr[nx][ny + 1] == 1) {
+                    while (ny + 1 < 100 && arr[nx][ny + 1] == 1) {
+                        ny++;
+                    }
+                }
+            }
+			System.out.printf("#%d %d%n", test_case, ny);
+		}
+    }    
+}
