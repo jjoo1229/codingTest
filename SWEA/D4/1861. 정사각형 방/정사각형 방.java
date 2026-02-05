8809a27d9b0061582ca935ca83d0c6f69bf3469b
jjoo1229 <110160016+jjoo1229@users.noreply.github.com>
2026-02-05 15:47:39 +0900

    [D4] Title: 정사각형 방, Time: 306 ms, Memory: 83,764 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -1,66 +1,63 @@
 import java.util.*;
-
+ 
 class Solution {
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for (int test_case = 1; test_case <= T; test_case++) {
-    		int n = sc.nextInt();
-    		int[][] graph = new int[n][n];
-    		for (int i = 0; i < n; i++) {
-        		for (int j = 0; j < n; j++) {
+    static int n;
+    static int[][] dp;
+    static int[][] graph;
+    
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int test_case = 1; test_case <= T; test_case++) {
+            n = sc.nextInt();
+            graph = new int[n][n];
+            dp = new int[n][n];
+            
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
                     graph[i][j] = sc.nextInt();
                 }
             }
             
-            Map<Integer, List<Integer>> room = new HashMap<>();
-            int max = 0;
             for (int i = 0; i < n; i++) {
-        		for (int j = 0; j < n; j++) {
-                    int count = move(i, j, n, graph);
-                    if (count >= max) {
-                        max = count;
-                        if (room.containsKey(count)) room.get(count).add(graph[i][j]);
-                        else {
-                            List<Integer> list = new ArrayList<>();
-                            list.add(graph[i][j]);
-                            room.put(count, list);
-                        }
+                for (int j = 0; j < n; j++) {
+                    solve(i, j);
+                }
+            }
+            
+            int max = 0;
+            int room = Integer.MAX_VALUE;
+            
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dp[i][j] > max) {
+                        max = dp[i][j];
+                        room = graph[i][j];
+                    } else if (dp[i][j] == max) {
+                        room = Math.min(room, graph[i][j]);
                     }
                 }
             }
-            int maxCount = Collections.max(room.keySet());
-            int value = Collections.min(room.get(maxCount));
-            System.out.printf("#%d %d %d%n", test_case, value, max);
-		}
-	}
-
-    public static int move(int x, int y, int n, int[][] graph) {
-        Queue<int[]> q = new LinkedList<>();
+            System.out.printf("#%d %d %d%n", test_case, room, max);
+        }
+    }
+ 
+    public static int solve(int x, int y) {
+        if (dp[x][y] != 0) return dp[x][y];
         
+        dp[x][y] = 1;
         int[] dx = {1, -1, 0, 0};
         int[] dy = {0, 0, 1, -1};
+             
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-        int count = 1;
-        q.offer(new int[]{x, y});
-
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int curX = cur[0];
-            int curY = cur[1];
-            
-            for (int i = 0; i < 4; i++) {
-                int nx = curX + dx[i];
-                int ny = curY + dy[i];
-
-                if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                if (graph[nx][ny] == graph[curX][curY] + 1) {
-                    q.offer(new int[]{nx, ny});
-                    count++;
-                    break;
-                }
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+            if (graph[nx][ny] == graph[x][y] + 1) {
+                dp[x][y] = 1 + solve(nx, ny);
             }
         }
-        return count;
+        return dp[x][y];
     }
 }
