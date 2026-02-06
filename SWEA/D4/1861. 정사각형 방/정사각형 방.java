247c8947b15e8a0dbe3368cd5ee871db04fff16c
jjoo1229 <110160016+jjoo1229@users.noreply.github.com>
2026-02-06 09:34:04 +0900

    [D4] Title: 정사각형 방, Time: 323 ms, Memory: 89,168 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -1,63 +1,57 @@
 import java.util.*;
- 
+  
 class Solution {
     static int n;
-    static int[][] dp;
     static int[][] graph;
-    
+    static boolean[] visited;
+     
     public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         for (int test_case = 1; test_case <= T; test_case++) {
             n = sc.nextInt();
             graph = new int[n][n];
-            dp = new int[n][n];
+            visited = new boolean[n * n + 1];
             
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
                     graph[i][j] = sc.nextInt();
                 }
             }
-            
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    solve(i, j);
-                }
-            }
-            
-            int max = 0;
+            int count = 0, answer = 0;
             int room = Integer.MAX_VALUE;
-            
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (dp[i][j] > max) {
-                        max = dp[i][j];
-                        room = graph[i][j];
-                    } else if (dp[i][j] == max) {
-                        room = Math.min(room, graph[i][j]);
+                    int roomNo = graph[i][j];
+                    if (visited[roomNo]) continue;
+                    count = solve(i,j);
+                    if (count == answer) {
+                        room = Math.min(room, roomNo);
+                    } else if (count > answer) {
+                        answer = count;
+                        room = roomNo;
                     }
                 }
             }
-            System.out.printf("#%d %d %d%n", test_case, room, max);
+
+            System.out.printf("#%d %d %d%n", test_case, room, answer);
         }
     }
- 
+  
     public static int solve(int x, int y) {
-        if (dp[x][y] != 0) return dp[x][y];
-        
-        dp[x][y] = 1;
+        visited[graph[x][y]] = true;
         int[] dx = {1, -1, 0, 0};
         int[] dy = {0, 0, 1, -1};
-             
+              
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-
+ 
             if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
             if (graph[nx][ny] == graph[x][y] + 1) {
-                dp[x][y] = 1 + solve(nx, ny);
+                return solve(nx, ny) + 1;
             }
         }
-        return dp[x][y];
+        return 1;
     }
 }
