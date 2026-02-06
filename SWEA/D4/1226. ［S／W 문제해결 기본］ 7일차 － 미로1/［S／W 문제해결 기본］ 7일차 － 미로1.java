f3e3bf1d23241307d03cc64c640fadeb0b1ec86b
jjoo1229 <110160016+jjoo1229@users.noreply.github.com>
2026-02-06 11:44:28 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 86 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -2,54 +2,53 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-	public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[][] map = new int[16][16];
+    static int startX;
+    static int startY;
+    static int arriveX;
+    static int arriveY;
 
-		for (int test_case = 1; test_case <= 10; test_case++) {
-    		br.readLine();
-            int[][] graph = new int[16][16];
-            int startX = 0, startY = 0, endX = 0, endY = 0;
-            
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int test_case = 1; test_case <= 10; test_case++) {
+            br.readLine();
             for (int i = 0; i < 16; i++) {
                 String line = br.readLine();
                 for (int j = 0; j < 16; j++) {
-                    graph[i][j] = line.charAt(j) - '0';  // '0'을 빼서 문자 -> 숫자 변환
-                    if (graph[i][j] == 2) {
+                    map[i][j] = line.charAt(j) - '0';
+                    if (map[i][j] == 2) {
                         startX = i;
                         startY = j;
-                    } else if (graph[i][j] == 3) {
-                        endX = i;
-                        endY = j;
+                    } else if (map[i][j] == 3) {
+                        arriveX = i;
+                        arriveY = j;
                     }
                 }
             }
-            int answer = dfs(graph, startX, startY, endX, endY);
-            System.out.printf("#%d %d%n", test_case, answer);
-		}
-	}
+            System.out.printf("#%d %d%n", test_case, dfs());
+        }
+    }
 
-    public static int dfs(int[][] graph, int x1, int y1, int x2, int y2) {
-         int[] dx = {0, 0, 1, -1};
-         int[] dy = {1, -1, 0, 0};
-
-        boolean[][] visited = new boolean[16][16];
-
+    public static int dfs() {
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
         Stack<int[]> stack = new Stack<>();
-        stack.push(new int[]{x1, y1});
-        visited[x1][y1] = true;
-
-        while (!stack.isEmpty()) {
+        stack.push(new int[]{startX, startY});
+        boolean[][] visited = new boolean[16][16];
+        visited[startX][startY] = true;
+        
+        while (!stack.empty()) {
             int[] cur = stack.pop();
-            int x = cur[0];
-            int y = cur[1];
+            int curX = cur[0];
+            int curY = cur[1];
 
-            if (x == x2 && y == y2) return 1;
+            if (curX == arriveX && curY == arriveY) return 1;
             for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+                int nx = curX + dx[i];
+                int ny = curY + dy[i];
 
                 if (nx < 0 || nx >= 16 || ny < 0 || ny >= 16) continue;
-                if (!visited[nx][ny] && graph[nx][ny] != 1) {
+                if (!visited[nx][ny] && map[nx][ny] != 1) {
                     stack.push(new int[]{nx, ny});
                     visited[nx][ny] = true;
                 }
