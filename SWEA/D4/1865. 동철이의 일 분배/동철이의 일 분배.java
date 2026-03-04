e37237ae9049fb7e07892bc843849e90dbe2daf9
jjoo1229 <110160016+jjoo1229@users.noreply.github.com>
2026-03-04 11:14:12 +0900

    [D4] Title: 동철이의 일 분배, Time: 1,595 ms, Memory: 29,568 KB -BaekjoonHub

--- SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.java
+++ SWEA/D4/1865. 동철이의 일 분배/동철이의 일 분배.java
@@ -1,45 +1,43 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Solution {
 	public static int t, n;
-	public static double max;
-	public static int[][] map;
 	public static double answer;
+	public static int[][] work;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		t = Integer.parseInt(br.readLine());
-
-		for (int test_case = 1; test_case <= t; test_case++) {
+		for (int tc = 1; tc <= t; tc++) {
 			n = Integer.parseInt(br.readLine());
-			map = new int[n][n];
+			work = new int[n][n];
 			for (int i = 0; i < n; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < n; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
+					work[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 
+			answer = 0.0;
 			boolean[] visited = new boolean[n];
-			max = 0.0;
 			dfs(0, 1.0, visited);
-			System.out.printf("#%d %.6f%n", test_case, max * 100);
+			System.out.printf("#%d %f%n", tc, answer * 100);
 		}
 	}
 
-	public static void dfs(int index, double result, boolean[] visited) {
+	public static void dfs(int index, double val, boolean[] visited) {
+		if (val <= answer) return;
+		
 		if (index == n) {
-			max = Math.max(max, result);
+			answer = Math.max(val, answer);
 			return;
 		}
 
 		for (int i = 0; i < n; i++) {
 			if (!visited[i]) {
-				double newResult = result * map[index][i] / 100.0;
-				if (newResult <= max) continue;
 				visited[i] = true;
-				dfs(index + 1, newResult, visited);
+				dfs(index + 1, val * work[index][i] / 100.0, visited);
 				visited[i] = false;
 			}
 		}
