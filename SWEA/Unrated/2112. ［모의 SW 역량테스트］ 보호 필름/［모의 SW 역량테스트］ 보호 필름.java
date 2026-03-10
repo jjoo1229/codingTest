9834c0e4ec806ada947535a5334597f7ead827b7
jjoo1229 <110160016+jjoo1229@users.noreply.github.com>
2026-03-10 17:27:07 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 478 ms, Memory: 97,112 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Solution {
 	public static int t, d, w, k, count;
 	public static int[][] grid;
+	public static int[] rowState;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,54 +23,48 @@ public class Solution {
 					grid[i][j] = Integer.parseInt(st2.nextToken());
 				}
 			}
+			rowState = new int[d];
+			Arrays.fill(rowState, -1);
 			count = k; // 최댓값인 k로 초기화
-			if (checkQual(grid))
-				count = 0; // 처음부터 가능이면 바로 0
+			if (checkQual()) count = 0; // 처음부터 가능이면 바로 0
 			else {
-				dfs(0, 0, grid);
+				dfs(0, 0);
 			}
 			System.out.printf("#%d %d%n", tc, count);
 		}
 	}
 
 	// 해당 줄을 0으로 바꾸거나 1로 바꾸거나 그대로 간다 그리고 매번 가능한지 확인
-	public static void dfs(int index, int injectCnt, int[][] newGrid) {
-		if (injectCnt > count) return;  // 이미 count보다 많으면 그냥 리턴
+	public static void dfs(int index, int injectCnt) {
+		if (injectCnt >= count) return; // 이미 count보다 많으면 그냥 리턴
 
-		if (checkQual(newGrid)) {
-			count = Math.min(count, injectCnt);
+		if (index == d) {
+			if (checkQual()) {
+				count = Math.min(count, injectCnt);
+			}
 			return;
 		}
 
-		if (index == d) return; // 다 돌았으면 그냥 리턴
+		rowState[index] = -1;
+		dfs(index + 1, injectCnt); // 그대로 간다
 
-		dfs(index + 1, injectCnt, newGrid); // 그대로 간다
+		// 0으로 바꾸고 간다
+		rowState[index] = 0;
+		dfs(index + 1, injectCnt + 1);
 
-		int[] temp = new int[w]; // temp에 index번째 줄 저장
-		for (int i = 0; i < w; i++) {
-			temp[i] = newGrid[index][i];
-		}
-
-		for (int i = 0; i < w; i++) {
-			newGrid[index][i] = 0;
-		}
-		dfs(index + 1, injectCnt + 1, newGrid); // 0으로 바꾸고 간다
-
-		for (int i = 0; i < w; i++) {
-			newGrid[index][i] = 1;
-		}
-		dfs(index + 1, injectCnt + 1, newGrid); // 1로 바꾸고 간다
-
-		for (int i = 0; i < w; i++) {
-			newGrid[index][i] = temp[i]; // 원상복구
-		}
+		// 1로 바꾸고 간다
+		rowState[index] = 1;
+		dfs(index + 1, injectCnt + 1);
 	}
 
-	public static boolean checkQual(int[][] newGrid) {
+	public static boolean checkQual() {
 		for (int j = 0; j < w; j++) {
 			int curCount = 1;
 			for (int i = 1; i < d; i++) {
-				if (newGrid[i][j] == newGrid[i - 1][j]) { // 내 앞에 거랑 같으면
+//				rowState[i]가 -1이면 grid[i][j], 아니면 rowState[i] 값을 사용한다.
+				int prev = (rowState[i - 1] == -1) ? grid[i - 1][j] : rowState[i - 1];
+				int cur = (rowState[i] == -1) ? grid[i][j] : rowState[i];
+				if (cur == prev) {
 					curCount++;
 				} else {
 					if (curCount >= k)
@@ -78,8 +73,7 @@ public class Solution {
 						curCount = 1;
 				}
 			}
-			if (curCount < k)
-				return false;
+			if (curCount < k) return false;
 		}
 		return true;
 	}
