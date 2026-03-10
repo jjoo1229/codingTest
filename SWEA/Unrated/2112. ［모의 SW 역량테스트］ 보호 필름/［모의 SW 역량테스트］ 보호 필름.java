3c1ea61be6b3e190ed2cbd9b5965de6b38833d2a
jjoo1229 <110160016+jjoo1229@users.noreply.github.com>
2026-03-10 15:54:34 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 478 ms, Memory: 97,112 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -1,85 +1,86 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Solution {
-	public static int d, w, k, answer;
+	public static int t, d, w, k, count;
 	public static int[][] grid;
 
-	public static void main(String args[]) throws Exception {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
+		t = Integer.parseInt(br.readLine());
 
-		for (int test_case = 1; test_case <= T; test_case++) {
+		for (int tc = 1; tc <= t; tc++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			d = Integer.parseInt(st.nextToken());
 			w = Integer.parseInt(st.nextToken());
 			k = Integer.parseInt(st.nextToken());
 
 			grid = new int[d][w];
-
 			for (int i = 0; i < d; i++) {
 				StringTokenizer st2 = new StringTokenizer(br.readLine());
 				for (int j = 0; j < w; j++) {
 					grid[i][j] = Integer.parseInt(st2.nextToken());
 				}
 			}
-			answer = d;
-			if (check())
-				answer = 0;
-			else
-				dfs(0, 0);
-			System.out.printf("#%d %d%n", test_case, answer);
-		}
-	}
-
-	public static boolean check() {
-		if (k == 1)
-			return true;
-
-		for (int j = 0; j < w; j++) {
-			boolean pass = false;
-			int length = 1;
-
-			for (int i = 1; i < d; i++) {
-				if (grid[i - 1][j] == grid[i][j]) {
-					length++;
-				} else {
-					length = 1;
-				}
-
-				if (length >= k) {
-					pass = true;
-					break;
-				}
+			count = k; // 최댓값인 k로 초기화
+			if (checkQual(grid))
+				count = 0; // 처음부터 가능이면 바로 0
+			else {
+				dfs(0, 0, grid);
 			}
-			if (!pass) return false;
-
+			System.out.printf("#%d %d%n", tc, count);
 		}
-		return true;
 	}
 
-	public static void dfs(int rowIndex, int count) {
-		if (count >= answer) return; // count가 저장된 값보다 커지면 제외(가지치기)
-		
-		if (check()) {
-			answer = Math.min(answer, count);
+	// 해당 줄을 0으로 바꾸거나 1로 바꾸거나 그대로 간다 그리고 매번 가능한지 확인
+	public static void dfs(int index, int injectCnt, int[][] newGrid) {
+		if (injectCnt > count) return;  // 이미 count보다 많으면 그냥 리턴
+
+		if (checkQual(newGrid)) {
+			count = Math.min(count, injectCnt);
 			return;
 		}
-			
-		if (rowIndex == d) return;
-		
 
-		dfs(rowIndex + 1, count); // 아무것도 안 하고 넘어간다
-		
-		int[] temp = new int[w];
-		for (int i = 0; i < w; i++) temp[i] = grid[rowIndex][i]; // 원래 row 저장
-		
-		for (int i = 0; i < w; i++) grid[rowIndex][i] = 0;
-		dfs(rowIndex + 1, count + 1); // A로 바꿔준다
+		if (index == d) return; // 다 돌았으면 그냥 리턴
 
-		for (int i = 0; i < w; i++) grid[rowIndex][i] = 1;
-		dfs(rowIndex + 1, count + 1); // B로 바꿔준다
+		dfs(index + 1, injectCnt, newGrid); // 그대로 간다
 
-		for (int i = 0; i < w; i++) grid[rowIndex][i] = temp[i]; // 원상복구
+		int[] temp = new int[w]; // temp에 index번째 줄 저장
+		for (int i = 0; i < w; i++) {
+			temp[i] = newGrid[index][i];
+		}
+
+		for (int i = 0; i < w; i++) {
+			newGrid[index][i] = 0;
+		}
+		dfs(index + 1, injectCnt + 1, newGrid); // 0으로 바꾸고 간다
+
+		for (int i = 0; i < w; i++) {
+			newGrid[index][i] = 1;
+		}
+		dfs(index + 1, injectCnt + 1, newGrid); // 1로 바꾸고 간다
+
+		for (int i = 0; i < w; i++) {
+			newGrid[index][i] = temp[i]; // 원상복구
+		}
+	}
+
+	public static boolean checkQual(int[][] newGrid) {
+		for (int j = 0; j < w; j++) {
+			int curCount = 1;
+			for (int i = 1; i < d; i++) {
+				if (newGrid[i][j] == newGrid[i - 1][j]) { // 내 앞에 거랑 같으면
+					curCount++;
+				} else {
+					if (curCount >= k)
+						break; // 이미 k개 이상이니까 패스
+					else
+						curCount = 1;
+				}
+			}
+			if (curCount < k)
+				return false;
+		}
+		return true;
 	}
 }
