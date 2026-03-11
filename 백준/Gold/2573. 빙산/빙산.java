ff6299736f5baf848975dc49f0968b3aace06218
jjoo1229 <110160016+jjoo1229@users.noreply.github.com>
2026-03-11 18:00:33 +0900

    [Gold IV] Title: 빙산, Time: 588 ms, Memory: 77752 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -1,91 +1,116 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static int n;
-	public static int m;
-	public static int[][] map;
-	public static int[] dx = { 0, 0, 1, -1 };
-	public static int[] dy = { 1, -1, 0, 0 };
+	public static int n, m;
+	public static int[][] grid;
+	public static int[][] minusGrid;
+	public static int[] dx = { 0, 0, -1, 1 };
+	public static int[] dy = { -1, 1, 0, 0 };
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		m = sc.nextInt();
-		map = new int[n][m];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		grid = new int[n][m];
+		minusGrid = new int[n][m];
+
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st2 = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+				grid[i][j] = Integer.parseInt(st2.nextToken());
+			}
+		}
+
+		int year = 0;
+		while (true) {
+			for (int i = 0; i < n; i++) {
+				Arrays.fill(minusGrid[i], 0); // 초기화
+			}
+			// 전체를 돌면서 녹는 애들 체크
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					if (grid[i][j] > 0) {
+						solve(i, j);
+					}
+				}
+			}
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					grid[i][j] = (grid[i][j] - minusGrid[i][j] < 0) ? 0 : grid[i][j] - minusGrid[i][j];
+				}
+			}
+			year++;
+			if (moreThanTwoIce() || allMelt()) break;
+		}
+		System.out.print(allMelt() ? 0 : year);
+	}
+
+	public static void solve(int x, int y) {
+		int count = 0;
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+				continue;
+			if (grid[nx][ny] == 0) {
+				count++;
+			}
+		}
+		minusGrid[x][y] = count;
+	}
+
+	public static boolean moreThanTwoIce() {
+		int count = 0;
+		boolean[][] visited = new boolean[n][m];
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
-				map[i][j] = sc.nextInt();
-			}
-		}
-		int time = 0;
-		int iceNum = 0;
-		while (true) {
-			// 빙산 녹이기
-			int[][] melt = new int[n][m]; // 각 칸에서 녹아야 하는 만큼 저장
-			ArrayList<int[]> list = new ArrayList<>(); // 녹을 자리 저장용
-
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
-					if (map[i][j] != 0) {
-						int count = 0;
-						for (int d = 0; d < 4; d++) {
-							int nx = i + dx[d];
-							int ny = j + dy[d];
-
-							if (nx < 0 || nx >= n || ny < 0 || ny >= m)
-								continue;
-							if (map[nx][ny] == 0)
-								count++;
-						}
-						melt[i][j] = (map[i][j] - count > 0) ? count : map[i][j];
-						list.add(new int[] { i, j });
-					}
+				if (!visited[i][j] && grid[i][j] > 0) {
+					bfs(i, j, visited);
+					count++;
+					if (count > 2)
+						return true;
 				}
 			}
-			for (int[] cur : list) { // 한꺼번에 녹이기
-				int curX = cur[0], curY = cur[1];
-				map[curX][curY] -= melt[curX][curY];
-			}
-			time++;
-
-			// 빙산 개수 구하기
-			iceNum = 0;
-			boolean[][] visited = new boolean[n][m];
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
-					if (!visited[i][j] && map[i][j] != 0) {
-						bfs(i, j, visited);
-						iceNum++;
-					}
-				}
-			}
-			if (iceNum > 1 || iceNum == 0)
-				break;
 		}
-		System.out.print(iceNum == 0 ? 0 : time);
+		if (count >= 2) return true;
+		return false;
 	}
 
 	public static void bfs(int x, int y, boolean[][] visited) {
-		Queue<int[]> q = new LinkedList<>();
-		q.offer(new int[] { x, y });
+		Queue<int[]> q = new ArrayDeque<int[]>();
 		visited[x][y] = true;
+		q.offer(new int[] { x, y });
 
 		while (!q.isEmpty()) {
 			int[] cur = q.poll();
 			int curX = cur[0];
 			int curY = cur[1];
 
-			for (int d = 0; d < 4; d++) {
-				int nx = curX + dx[d];
-				int ny = curY + dy[d];
+			for (int i = 0; i < 4; i++) {
+				int nx = curX + dx[i];
+				int ny = curY + dy[i];
 
-				if (nx < 0 || nx > n || ny < 0 || ny >= m)
+				if (nx < 0 || nx >= n || ny < 0 || ny >= m)
 					continue;
-				if (!visited[nx][ny] && map[nx][ny] != 0) {
+				if (!visited[nx][ny] && grid[nx][ny] != 0) {
 					visited[nx][ny] = true;
 					q.offer(new int[] { nx, ny });
+
 				}
 			}
 		}
+	}
+
+	public static boolean allMelt() {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (grid[i][j] != 0)
+					return false;
+			}
+		}
+		return true;
 	}
 }
